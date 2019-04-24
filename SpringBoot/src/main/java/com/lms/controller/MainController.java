470a3038b9cad33e90ce2d1c209a542b8452e1c4
Nitin Rajale <nitinrajale5@gmail.com>
2019-04-24 23:11:12 +0530

    test

---
+++ SpringBoot/src/main/java/com/lms/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.lms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping()
+	private String hello()
+	{
+		return null;
+	}
+	
+
+}
